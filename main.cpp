2ab3b05738a8a42f60e5355b59af674b6d0d8939
JonasIKN2 <jonasna93@gmail.com>
2018-04-04 15:27:55 +0200

    minor changes

--- main.cpp
+++ main.cpp
@@ -10,12 +10,15 @@
 using namespace boost::asio;
 using ip::tcp;
 
+const int BUFF_SIZE = 1000;
+
 std::string read_server(tcp::socket& server_sock);
 unsigned long write_server(tcp::socket& server_sock, const char* buf, unsigned long count);
 unsigned long write_server(tcp::socket& server_sock, const std::string& msg);
 void receive_file(tcp::socket& server_sock, const std::string& filename);
 void output_percentage(unsigned long current, unsigned long target);
 void output_percentage2(unsigned long current, unsigned long target);
+
 int main(int argc, char** argv)
 {
     try
@@ -25,11 +28,14 @@ int main(int argc, char** argv)
             throw std::invalid_argument("Usage: <file_client> <server_ip> <file_path>");
         }
 
+        auto ip = argv[1];
+
         io_service io_service;
 
         tcp::socket sock(io_service);
         tcp::resolver resolver(io_service);
-        connect(sock, resolver.resolve(argv[1], "9000"));
+        //connect(sock, resolver.resolve(argv[1], "9000"));
+        connect(sock, resolver.resolve(tcp::endpoint(ip::address_v4::from_string(ip), 9000)));
 
         receive_file(sock, argv[2]);
 
@@ -77,31 +83,34 @@ void receive_file(tcp::socket& server_sock, const std::string& filepath)
         std::ofstream output_file(file_name);
 
         unsigned long bytes_received = 0;
-        char input_buffer[1000];
+        char input_buffer[BUFF_SIZE];
 
-        auto start = chrono::steady_clock::now();
+        //auto start = std::chrono::steady_clock::now();
 
         while(auto count = read_server(server_sock, input_buffer))
         {
             bytes_received += count;
             output_file.write(input_buffer, count);
-            //output_percentage2(bytes_received, file_size);
+            //output_percentage(bytes_received, file_size);
+            //count = read_server(server_sock, input_buffer);
         }
 
         output_file.close();
 
-        auto end = chrono::steady_clock::now();
-        auto diff = end - start;
+        //auto end = std::chrono::steady_clock::now();
+        //auto diff = end - start;
         //std::cout << std::endl;
 
-        std::cout << "All done! - received: " << bytes_received << " bytes out of " << file_size << std::endl;
-        std::cout << "Download took " << chrono::duration<double, std::milli>(diff).count() / 1000 << " sec " << std::endl;
+        std::cout << "All done! - received: " << bytes_received << " bytes out of " << file_size << ". " << file_size - bytes_received << " remaining..." << std::endl;
+        //std::cout << "Download took " << std::chrono::duration<double, std::milli>(diff).count() / 1000 << " sec " << std::endl;
 
     }else
     {
         std::cout << "File was not found on the server!" << std::endl;
     }
 }
+
+// Nedenstående funktioner er blot for at lave sjovt output...
 
 void output_percentage(unsigned long current, unsigned long target)
 {
--- read_server.h
+++ read_server.h
@@ -7,10 +7,15 @@ template<size_t N>
 unsigned long read_server(boost::asio::ip::tcp::socket& sock, char(&buff)[N])
 {
     using namespace boost::asio;
-
     boost::system::error_code ignored_error;
-    auto n = boost::asio::read(sock, buffer(buff), transfer_exactly(N), ignored_error);
-    return n;
+
+    auto n = boost::asio::read(sock, buffer(&buff[0], N), ignored_error);
+    if(ignored_error)
+    {
+        std::cout << "Error! n is " << n << " " << ignored_error.message() << std::endl;
+    }
+
+    return (unsigned long)n;
 }
 
 #endif // READ_SERVER_H
